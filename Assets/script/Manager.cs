bdaa82df0156b7da24b87a084ee62b5ed684cf4d
Relz <relz0071@gmail.com>
2017-03-12 08:46:03 +0300

    Создан проект "Крестики-Нолики". Добавлено игровое поле, кнопки выбора роли, кнопка "Начать заново" и строка состояния.

---
+++ Assets/script/Manager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public enum PlayerRole
+{
+	None,
+	Cross,
+	Circle
+}
+
+public class Manager : MonoBehaviour 
+{
+	public static PlayerRole firstPlayerRole = PlayerRole.None;
+}
---
+++ Assets/script/PlayerChooseRole.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI; 
+
+public class PlayerChooseRole : MonoBehaviour 
+{
+	public Button btnChooseCross;
+	public Button btnChooseCircle;
+	// Use this for initialization
+	void Start () 
+	{
+		ChooseCross();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	}
+
+	public void OnBtnChooseCrossClick () 
+	{
+		DisableChooseRoleButtons();
+		ChooseCross();
+	}
+	
+	public void OnBtnChooseCircleClick () 
+	{
+		DisableChooseRoleButtons();
+		ChooseCircle();
+	}
+
+	public void ChooseCross () 
+	{
+		Manager.firstPlayerRole = PlayerRole.Cross;
+		Debug.Log("Cross");
+	}
+	
+	public void ChooseCircle () 
+	{
+		Manager.firstPlayerRole = PlayerRole.Circle;
+		Debug.Log("Circle");
+	}
+
+	private void DisableChooseRoleButtons()
+	{
+		btnChooseCross.interactable = false;
+		btnChooseCross.enabled = false;
+		btnChooseCircle.interactable = false;
+		btnChooseCircle.enabled = false;
+	}
+}
